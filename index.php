057f02946ef61f8142a8126fb2d0c11966c3b31f
jetdocument <jetdocument@gmail.com>
2017-03-30 11:58:06 +0700

    date

--- index.php
+++ index.php
@@ -12,7 +12,9 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 
-			$event['message']['text'] = 'กูตอบมึงละนะ';
+			$today = getdate();
+
+			$event['message']['text'] = $today;
 
 			// Get text sent
 			$text = $event['message']['text'];

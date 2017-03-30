9ef1610aba46eda228b635ec9ef661ecd1982df4
jetdocument <jetdocument@gmail.com>
2017-03-30 10:08:41 +0700

    building

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 
-			$event['message'] = 'What';
+			$event['message'] = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');;
 
 			// Get text sent
 			$text = $event['message']['text'];

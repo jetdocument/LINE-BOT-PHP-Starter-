baca993b03cbeb41b0c2b83e8201451072ab2a3b
jetdocument <jetdocument@gmail.com>
2017-03-30 10:57:48 +0700

    debug

--- getprofile.php
+++ getprofile.php
@@ -1,4 +1,5 @@
 <?php
+echo "Something";
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('lGIPn6XGSZmb1qYpU/kTWGADC/7keEWP0kI9ybaQjtRyaeyflX5b1QEIwcz3itdl4WtWCtB3cB4zG0RRpU+SXzF4j6XEy4V7sliVfu5pfJd8LyPhGuqNkN2KFHjLElwc6ReLkJOIP5v7/msSD0KUxQdB04t89/1O/w1cDnyilFU=');
 echo "Access token";
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '41a0536c6ac1d4202c8a7c867728c933']);

a70008b0ceea1ec64b357751e2a5d69d971b3603
jetdocument <jetdocument@gmail.com>
2017-04-12 10:12:49 -0400

    Push Changed

--- push.php
+++ push.php
@@ -1,13 +1,20 @@
 <?php
-$ composer require linecorp/line-bot-sdk
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('lGIPn6XGSZmb1qYpU/kTWGADC/7keEWP0kI9ybaQjtRyaeyflX5b1QEIwcz3itdl4WtWCtB3cB4zG0RRpU+SXzF4j6XEy4V7sliVfu5pfJd8LyPhGuqNkN2KFHjLElwc6ReLkJOIP5v7/msSD0KUxQdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '41a0536c6ac1d4202c8a7c867728c933']);
 
-	$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-	$response = $bot->pushMessage('ude008f1b55bb49dfd64dd2cb7d5471c7', $textMessageBuilder);
+<?php
+include ('bot.php');
+$channelSecret = '41a0536c6ac1d4202c8a7c867728c933';
+$access_token  = 'lGIPn6XGSZmb1qYpU/kTWGADC/7keEWP0kI9ybaQjtRyaeyflX5b1QEIwcz3itdl4WtWCtB3cB4zG0RRpU+SXzF4j6XEy4V7sliVfu5pfJd8LyPhGuqNkN2KFHjLElwc6ReLkJOIP5v7/msSD0KUxQdB04t89/1O/w1cDnyilFU=';
+$bot = new BOT_API($channelSecret, $access_token);
+	
+$bot->sendMessageNew('Ua19821cd93141008d26221f16381d256', 'Hello World !!');
+if ($bot->isSuccess()) {
+    echo 'Succeeded!';
+    exit();
+}
+// Failed
+echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
+exit();
 
-	echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-
-echo "Ready";
+echo "OK!";
 
 ?>

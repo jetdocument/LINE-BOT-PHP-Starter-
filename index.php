7d4b8323745163e95339d120ea98dbcf83ca0c7c
jetdocument <jetdocument@gmail.com>
2017-04-20 00:20:28 -0400

    echo index

--- index.php
+++ index.php
@@ -5,6 +5,19 @@ include ('bot.php');
 $channelSecret = '41a0536c6ac1d4202c8a7c867728c933';
 $access_token  = 'lGIPn6XGSZmb1qYpU/kTWGADC/7keEWP0kI9ybaQjtRyaeyflX5b1QEIwcz3itdl4WtWCtB3cB4zG0RRpU+SXzF4j6XEy4V7sliVfu5pfJd8LyPhGuqNkN2KFHjLElwc6ReLkJOIP5v7/msSD0KUxQdB04t89/1O/w1cDnyilFU=';
 
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
+
 $bot = new BOT_API($channelSecret, $access_token);
 	
 if (!empty($bot->isEvents)) {
@@ -20,6 +33,6 @@ if (!empty($bot->isEvents)) {
 	echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
 	exit();
 }
-echo json_encode($bot->events);
+
 
 ?>

cd8ae6945fe7b89702468cfaa884adcdd2ea0af3
jetdocument <jetdocument@gmail.com>
2017-04-20 04:22:50 -0400

    echo index

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ if (!empty($bot->isEvents)) {
 	#$bot->replyMessageNew($bot->replyToken, json_encode($bot->events));
 	#$bot->replyMessageNew($bot->replyToken, json_encode($bot->events));
 	$message = json_decode($bot->message, true);
-	$bot->replyMessageNew($bot->replyToken, $message});
+	$bot->replyMessageNew($bot->replyToken, $message);
 	#$bot->replyMessageNew($bot->replyToken, json_encode($bot->source));
 	#$bot->replyMessageNew($bot->replyToken, json_encode($bot->replyTroken));
 	#$bot->replyMessageNew($bot->replyToken, json_encode($bot->timestamp)); 

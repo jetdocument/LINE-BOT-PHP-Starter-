14b16a78d5192dcc1edaa0b4b8570b35dde2d4d3
jetdocument <jetdocument@gmail.com>
2017-04-20 16:57:12 +0700

    Group

--- push.php
+++ push.php
@@ -6,7 +6,6 @@ $channelSecret = '41a0536c6ac1d4202c8a7c867728c933';
 $access_token  = 'lGIPn6XGSZmb1qYpU/kTWGADC/7keEWP0kI9ybaQjtRyaeyflX5b1QEIwcz3itdl4WtWCtB3cB4zG0RRpU+SXzF4j6XEy4V7sliVfu5pfJd8LyPhGuqNkN2KFHjLElwc6ReLkJOIP5v7/msSD0KUxQdB04t89/1O/w1cDnyilFU=';
 $bot = new BOT_API($channelSecret, $access_token);
 $bot->sendMessageNew(
-	'Ua19821cd93141008d26221f16381d256',
 	'Ccc6c182dace6aeda58b1b7dc24276da7', 
 	'Hello World !!');
 if ($bot->isSuccess()) {
